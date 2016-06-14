64a810805dfcb570bcfc644276d6ca90d8a3fd35
Scorpionsc <petruninnikolay@gmail.com>
2016-06-14 15:38:12 +0300

    masked input

--- app/ts/components/mobile.component.ts
+++ app/ts/components/mobile.component.ts
@@ -9,7 +9,6 @@ import { Component, Output, EventEmitter, Input } from 'angular2/core';
 
 export class MobileComponent {
     @Input() mobileNumber: string;
-    @Output() changeMobileNumber = new EventEmitter();
     @Output() showKeyboard = new EventEmitter();
 
     content = {
@@ -21,12 +20,13 @@ export class MobileComponent {
         ]
     };
 
-    onChangeMobileNumber(){
-        this.changeMobileNumber.emit( this.mobileNumber );
-    }
+    onClickInput( event,type ){
+        let data = {
+                input: <HTMLInputElement>event.target.parentElement.getElementsByTagName('input')[0],
+                type: type
+            };
 
-    onClickInput( type ){
-        this.showKeyboard.emit( type );
+        this.showKeyboard.emit( data );
     }
 
 
